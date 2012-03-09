8bfa525ea3fb2204f02285bea92e0bfa9e4c12ee
Chirayu Desai <cdesai@cyanogenmod.org>
2012-03-09 16:37:06 +0530

    add support for p1
    Signed-off-by: Chirayu Desai <cdesai@cyanogenmod.org>
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Signed-off-by: Koudai Aono <koxudaxi@gmail.com>
    Change-Id: I2dcbaee7f17852ed3ea4a5db2d103704027fe259

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -198,4 +198,12 @@ extern struct platform_device s3c_device_ac97;
 extern void *s3c_set_platdata(void *pd, size_t pdsize,
 			      struct platform_device *pdev);
 
+extern struct platform_device s5p_device_tvout;
+extern struct platform_device s5p_device_cec;
+extern struct platform_device s5p_device_hpd;
+
+#if defined(CONFIG_VIDEO_TSI)
+extern struct platform_device s3c_device_tsi;
+#endif
+
 #endif /* __PLAT_DEVS_H */
