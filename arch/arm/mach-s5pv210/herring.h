0d6cd409142091fa2f616c6f7c78788cf178b85e
Chirayu Desai <chirayudesai1@gmail.com>
2012-03-09 16:37:06 +0530

    added support for p1
    Change-Id: Ic7641f158a5cbad0422753deabf92bccff34d643

--- arch/arm/mach-s5pv210/herring.h
+++ arch/arm/mach-s5pv210/herring.h
@@ -33,6 +33,12 @@ extern void s3c_setup_uart_cfg_gpio(unsigned char port);
 # define phone_is_aries_cdma() (0)
 #endif
 
+#ifdef CONFIG_PHONE_P1_CDMA
+# define phone_is_p1_cdma() (1)
+#else
+# define phone_is_p1_cdma() (0)
+#endif
+
 extern struct s5p_panel_data herring_panel_data;
 extern struct s5p_tft_panel_data herring_sony_panel_data;
 extern struct s5p_tft_panel_data herring_hydis_panel_data;
